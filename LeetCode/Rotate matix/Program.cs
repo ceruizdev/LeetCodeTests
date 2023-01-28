e291679872717a08450e8972fd6f9e622af704f5
Carlos Ruiz <ceruiz.dev@gmail.com>
2023-01-28 15:06:04 -0500

    Class updated with functionality

--- LeetCode/Rotate matix/Program.cs
+++ LeetCode/Rotate matix/Program.cs
@@ -1,25 +1,50 @@
 ﻿// See https://leetcode.com/problems/rotate-image/
 
 Solution sol = new Solution();
-int[,] matrix = new int [,] {
-    { 1, 2, 3 },
-    { 4, 5, 6 },
-    { 7, 8, 9 }
+int[][] matrix = new int[][] { 
+    new int[] { 1,2,3 },
+    new int[] { 4,5,6 },
+    new int[] { 7,8,9 },
+    //{ 7, 4, 1 },
+    //{ 8, 5, 2 },
+    //{ 9, 6, 3 }
 };
+
 sol.Rotate(matrix);
+
+
+
 public class Solution
 {
-    public void Rotate(int[,] matrix)
+
+    public void Rotate(int[][] matrix)
     {
-        
-        int x = matrix.GetLength(0);
-        int y = matrix.GetLength(1);
-        int[,] matrix2 = new int[x,y];
-        for (int i = 0; i <= x - 1; i++) {
-            for (int j = 0; j <= y - 1 ; j++)
+        int size = matrix.GetLength(0);
+        int yAux = 0;
+        int xAux;
+
+        int[,] tempMatrix = new int[size, size];
+
+        for (int i = 0; i <= size - 1; i++)
+        {
+            xAux = size - 1;
+            for (int j = 0; j <= size - 1; j++)
             {
-                // TODO
-                Console.Write($"{matrix[i, j]}");
+                tempMatrix[i,j] = matrix[xAux][yAux];
+                xAux--;
+            }
+            yAux++;
+        }
+        PrintMatrix(tempMatrix, size);
+    }
+
+
+    public void PrintMatrix(int[,] matrix, int size) {
+        for (int i = 0; i <= size - 1; i++)
+        {
+            for (int j = 0; j <= size - 1; j++)
+            {
+                Console.Write($" {matrix[i,j]} ");
             }
             Console.WriteLine("");
         }
